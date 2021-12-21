1cca8e09dc6aabb568ae872d88d051f85a2742ec
unknown <huyvdph12786@fpt.edu.vn>
2021-12-21 15:13:37 +0700

    up react

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -1,16 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Fmarket</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
+
 <body>
-    @yield('content')
+    <div id="app">
+        @yield('content')
 
+    </div>
 
+    @include('layouts.js')
     @yield('page-script')
 </body>
+
 </html>
